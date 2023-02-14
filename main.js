ccf528365f9f48df826c02974e481b2a641c880d
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-02-14 17:56:56 -0300

    Alura Midi Curso
    Curso Alura

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom(idElementAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
@@ -15,7 +13,12 @@ while (contador < listaDeTeclas.length) {
     tocaSom(idAudio);
   };
 
-  contador = contador + 1;
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter" ) {
+    tecla.classList.add("ativa");
+  };
 
-  console.log(contador);
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
