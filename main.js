e90fb43764090a8595b929f0cfc07997ddd97b00
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-02-14 21:05:20 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-function tocaSom(idElementAudio) {
-  document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    //alert('Elemento não encontrado');
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-for (let contador = 0; listaDeTeclas.length; contador++) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`;
+  const idAudio = `#som_${instrumento}`; //template string
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
 
   tecla.onkeydown = function (evento) {
-    if (evento.code === "Space" || evento.code === "Enter" ) {
-    tecla.classList.add("ativa");
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
   };
 
   tecla.onkeyup = function () {
